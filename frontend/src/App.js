858c8c6b513796e09b22dbe0353d1dd22e6fb2a4
Ushani Sanjana <ushsanjana@gmail.com>
2025-03-17 23:12:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom"; // No need to import BrowserRouter again
+import { Routes, Route } from "react-router-dom"; 
 import Login from "./pages/loginPage";
 import SignUp from "./pages/signUpPage";
 import Dashboard from "./pages/dashboard";
